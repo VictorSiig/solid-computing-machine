3de05f945fcdacf502788708fb92e3d4fb4faee5
Victor Siig <victorsiig@live.dk>
2020-04-20 11:34:21 +0200

    init commit

--- Main.java
+++ Main.java
@@ -3,5 +3,11 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World of Git!");
+        System.out.println("Endnu en linie");
+
+        int x = 5;
+        for (int i= 0; i < 3; i++)
+            x += 8;
+        System.out.println(x);
     }
 }
